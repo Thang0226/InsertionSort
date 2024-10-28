35b077491630ff5cda724cd38b544f5e35398810
Thang0226 <thang.nd0226@gmail.com>
2024-10-28 10:57:41 +0700

    add input array feature

--- src/InsertionSortByStep.java
+++ src/InsertionSortByStep.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class InsertionSortByStep {
 	static int[] list = {2, 3, 2, 5, 6, 1, -2, 3, 14, 12};
 
@@ -23,6 +25,15 @@ public class InsertionSortByStep {
 	}
 
 	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter list size: ");
+		int size = scanner.nextInt();
+		int[] list = new int[size];
+		System.out.println("Enter " + list.length + " values:");
+		for (int i = 0; i < list.length; i++) {
+			list[i] = scanner.nextInt();
+		}
+
 		System.out.println("List elements:");
 		for (int number : list) {
 			System.out.print(" " + number);
